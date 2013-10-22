5fa04ee0fa786d99fd7836a0c1f577f91316c3d8
Johann Bronsch <johann@bronsch.com>
2013-10-22 18:16:17 +0200

    Dummy Main created

---
+++ AnimeGame/src/animeGame/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package animeGame;
+
+/**
+ * @author Sascha Waltz
+ * @author Johann Bronsch
+ *
+ * @Team: uppercamelcase!
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
